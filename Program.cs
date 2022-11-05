0f1101d03ee3e57d13499b0248afa5d0380b0706
GFW_72 <116944430+TRR72@users.noreply.github.com>
2022-11-05 17:14:53 +0500

    HW05

---
+++ Program.cs
@@ -0,0 +1,109 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+
+int numberA = ReadInt("Введите число A: ");
+int numberB = ReadInt("Введите число B: ");
+ToDegree(numberA, numberB);
+
+void ToDegree(int a, int b) // Функция возведения в степень
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    Console.WriteLine(result);
+}
+
+int ReadInt(string message) // Функция ввода
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+*/
+
+
+/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+/*
+int number = ReadInt("Введите число: ");
+int len = NumberLen(number);
+SumNumbers(number, len);
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+// ПОДСЧЕТ ЦИФР
+int NumberLen(int a)
+{
+    int index = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index;
+}
+
+// вывод суммы цифр в числе
+void SumNumbers(int n, int len)
+{
+    int sum = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine(sum);
+}
+*/
+
+
+/*
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+
+*/
+ Start();
+
+
+ void Start()
+{
+
+
+    int size = SetNumber("size");
+    int minValue = SetNumber("minValue");
+    int maxValue = SetNumber("maxValue");
+    int[] array =GetRandonArray(size, minValue, maxValue);
+    Console.WriteLine($"[{String.Join(",", array)}]");
+}
+
+
+int[] GetRandonArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue);
+    }
+
+    return result;
+}
+
+int SetNumber(string numberName)
+{
+    Console.Write($"Enter number {numberName}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+ 
+
